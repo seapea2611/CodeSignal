d09f75432224f72694eb8c5dd7698f53c840b180
seapea2611 <seapea2002@gmail.com>
2023-08-12 15:10:48 +0700

    sua code

--- src/Intro/Bai7.java
+++ src/Intro/Bai7.java
@@ -9,15 +9,21 @@ public class Bai7 {
                 if (count > 1) {
                     return false;
                 }
-                if (i > 0 && i < sequence.length - 2
-                        && sequence[i - 1] >= sequence[i + 1]
-                        && sequence[i] >= sequence[i + 2]) {
-                    return false;
-                }
+            }
+        }
+        for (int i = 0; i <sequence.length ; i++) {
+            // 3,4,1,2
+            // kiểm tra xem xóa 1 số có biến dãy thành tăng dần k?
+            if (i < sequence.length - 3
+                    && sequence[i] >= sequence[i + 2]
+                    && sequence[i + 1] >= sequence[i + 3]) {
+                return false;
             }
         }
         return true;
+
     }
+
 
     public static void main(String[] args) {
 
